749ec9e12a7f5d2b062e2b5137f3aef021162fbe
Paulin K. Domfang <pk.domfang@gmail.com>
2022-10-17 20:01:50 +0200

    admin panel structure

--- myapp/apps/admin/src/app/app.module.ts
+++ myapp/apps/admin/src/app/app.module.ts
@@ -1,15 +1,32 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import * as path from 'path';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: ShellComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+        ],
+
+    },
+];
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, DashboardComponent],
-    imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
+    declarations: [AppComponent, NxWelcomeComponent, DashboardComponent, ShellComponent, SidebarComponent],
+    imports: [BrowserModule, RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })],
     providers: [],
     bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
