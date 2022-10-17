0b2d9b766136e5578e02cb3112d33485e64730be
Paulin K. Domfang <pk.domfang@gmail.com>
2022-10-17 17:44:46 +0200

    style apps implementation

--- myapp/apps/annualproject/src/app/app.component.ts
+++ myapp/apps/annualproject/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'myapp-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+
 })
 export class AppComponent {
   title = 'annualproject';
