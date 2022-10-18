f813df2a7a03777300458557d894f0b785eac3fc
Paulin K. Domfang <pk.domfang@gmail.com>
2022-10-18 14:59:21 +0200

    espace personnel structure basique

---
+++ myapp/apps/forfait-tree/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'forfait-tree-root',
+    templateUrl: './app.component.html',
+})
+export class AppComponent {
+    title = 'forfait-tree';
+}
---
+++ myapp/apps/forfait-two/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'forfait-two-root',
+    templateUrl: './app.component.html',
+})
+export class AppComponent {
+    title = 'forfait-two';
+}
