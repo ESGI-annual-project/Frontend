6bcead588cb388e7b7c2aa22039101e152eb80e5
Paulin K. Domfang <pk.domfang@gmail.com>
2022-10-21 15:11:29 +0200

    route user implementation

--- myapp/apps/admin/src/app/app.module.ts
+++ myapp/apps/admin/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CategoriesListComponent } from './pages/categories/categories-list/categories-list.component';
 import { ProductsListComponent } from './pages/products/products-list/products-list.component';
 import { ProductsFormComponent } from './pages/products/products-form/products-form.component';
+import { UsersFormComponent } from './pages/users/users-form/users-form.component';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
 
 import { CardModule } from 'primeng/card';
 import { ToolbarModule } from 'primeng/toolbar';
@@ -30,6 +32,9 @@ import { DropdownModule } from 'primeng/dropdown';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { EditorModule } from 'primeng/editor';
+import { TagModule } from 'primeng/tag';
+import { InputMaskModule } from 'primeng/inputmask';
+
 
 
 const UX_MODULE = [
@@ -45,7 +50,9 @@ const UX_MODULE = [
     DropdownModule,
     InputTextareaModule,
     InputSwitchModule,
-    EditorModule
+    EditorModule,
+    TagModule,
+    InputMaskModule
 ];
 
 const routes: Routes = [
@@ -80,6 +87,18 @@ const routes: Routes = [
             {
                 path: 'products/form/:id',
                 component: ProductsFormComponent
+            },
+            {
+                path: 'users',
+                component: UsersListComponent
+            },
+            {
+                path: 'users/form',
+                component: UsersFormComponent
+            },
+            {
+                path: 'users/form/:id',
+                component: UsersFormComponent
             }
         ]
     }
@@ -95,7 +114,9 @@ const routes: Routes = [
         CategoriesListComponent,
         CategoriesFormComponent,
         ProductsListComponent,
-        ProductsFormComponent
+        ProductsFormComponent,
+        UsersFormComponent,
+        UsersListComponent
     ],
     imports: [
         BrowserModule,
